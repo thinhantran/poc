b325082c8f663bcacfc9135a94c04f9cac9fe3e4
thinhan_tran <thinhan_tran@ens.univ-artois.fr>
2025-07-15 00:13:50 +0200

    ajouter host

--- JoinUs/astro.config.mjs
+++ JoinUs/astro.config.mjs
@@ -6,4 +6,9 @@ export default defineConfig({
     output: 'server',
     adapter: node({ mode: 'standalone' }),
     integrations: [vue()],
+    vite: {
+        server: {
+            allowedHosts: ['poc-front-2.onrender.com'],
+        },
+    },
 });
