e80f5d0765aafffb76627b3d1de8ca021bc1d4a2
thinhan_tran <thinhan_tran@ens.univ-artois.fr>
2025-07-14 23:42:25 +0200

    output static

--- JoinUs/astro.config.mjs
+++ JoinUs/astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import staticAdapter from '@astrojs/static';
 
 export default defineConfig({
     output: 'static',
-    adapter: staticAdapter(),
     integrations: [vue()],
 });
