b7169afc25911fa53eb273c35433b18e7c5fe7c0
thinhan_tran <thinhan_tran@ens.univ-artois.fr>
2025-07-15 01:22:05 +0200

    test debug cors

--- JoinUsAPI/src/main/java/com/conference/joinusapi/config/WebConfig.java
+++ JoinUsAPI/src/main/java/com/conference/joinusapi/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(getAllowedOrigins())
+                        .allowedOrigins("https://poc-front-2.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
